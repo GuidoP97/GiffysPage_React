badd8f1d64bc1246634bfd7535feddf4de78fa97
GuidoP97 <guidopodesta99@gmail.com>
2021-03-30 17:51:22 -0300

    Add hook useContext

--- src/App.jsx
+++ src/App.jsx
@@ -3,28 +3,37 @@ import './App.css'
 import Home from './pages/Home/Home'
 import SearchResults from './pages/SearchResults/SearchResults'
 import Detail from './pages/Detail/Detail'
+import StaticContext from './context/StaticContext'
 
 import { Link, Route } from "wouter"
 
 export default function App() {
+  // Todos lo que envuelve StaticContext tiene acceso a el.
+  // Si se utiliza estas obligado a pasarle un value
   return (
-    <div className="App">
-      <section className="App-content">
-        <Link to="/">
-          <img className="App-logo" alt='Giffy logo' src='/logo.png' />
-        </Link>
-        <Route
-          component={Home}
-          path="/"
-        />
-        <Route
-          component={SearchResults}
-          path="/search/:keyword"  />
-        <Route
-          component={Detail}
-          path="/gif/:id"
-        />
-      </section>
-    </div>
+    <StaticContext.Provider value={{
+      name: 'guidacho99',
+      followRepo: true
+    }}
+    >
+      <div className="App">
+        <section className="App-content">
+          <Link to="/">
+            <img className="App-logo" alt='Giffy logo' src='/logo.png' />
+          </Link>
+          <Route
+            component={Home}
+            path="/"
+          />
+          <Route
+            component={SearchResults}
+            path="/search/:keyword"  />
+          <Route
+            component={Detail}
+            path="/gif/:id"
+          />
+        </section>
+      </div>
+    </StaticContext.Provider>
   )
 }
