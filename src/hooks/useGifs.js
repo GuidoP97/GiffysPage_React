1e1bf59751d7940a66f6da21551e8accbcb501cf
GuidoP97 <guidopodesta99@gmail.com>
2021-03-30 17:17:43 -0300

    create custom hook useGifs

---
+++ src/hooks/useGifs.js
@@ -0,0 +1,25 @@
+import { useState, useEffect } from "react"
+import getGifs from '../services/getGifs'
+
+function useGifs({ keyword } = { keyword: null }) {
+  const [loading, setLoading] = useState(false)
+  const [gifs, setGifs] = useState([])
+  
+  useEffect(function () {
+    setLoading(true)
+    // recuperamos la keyword de localStorage
+    const keywordToUse = keyword || localStorage.getItem('lastKeyword')
+
+    getGifs({ keyword: keywordToUse })
+    .then(gifs => {
+      setGifs(gifs)
+      setLoading(false)
+      // Guardamos la keyword en localStorage
+      localStorage.setItem('lastKeyword', keyword)
+      })
+  }, [keyword])
+
+  return {loading, gifs}
+}
+
+export default useGifs
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from "react"
+import { Link, useLocation } from "wouter"
+import ListOfGifs from "../../components/ListOfGifs/ListOfGifs"
+import useGifs from '../../hooks/useGifs'
+
+const POPULAR_GIFS = ["Matrix","Venezuela", "Chile", "Colombia", "Ecuador"]
+
+export default function Home() {
+  const [keyword, setKeyword] = useState('')
+  const [ path, pushLocation ] =  useLocation()
+  const { loading, gifs } = useGifs()
+
+  const handleSubmit = event => {
+    event.preventDefault()
+    // navegar a otra ruta
+    pushLocation(`/search/${keyword}`)
+  }
+  
+  const handleChange = event => {
+    setKeyword(event.target.value)
+  }
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <input 
+          defaultValue={keyword} 
+          onChange={handleChange}
+          placeholder="Search a gif here ..."
+          type="text" 
+        />
+        <button>Buscar</button>
+      </form>
+      <h3 className="App-title">Última búsqueda</h3>
+      <ListOfGifs gifs={gifs} />
+      <h3 className="App-title">Los gifs más populares</h3>
+      <ul>
+      {POPULAR_GIFS.map((popularGif) => (
+        <li key={popularGif}>
+          <Link to={`/search/${popularGif}`}>Gifs de {popularGif}</Link>
+        </li>
+      ))}
+      </ul>
+    </>
+  )
+}
